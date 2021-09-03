210a300e805e9ea33a5b4afe218c8b11c99e670d
alexnavtt <alexnavtt@utexas.edu>
2021-09-02 21:09:36 -0500

    Cleaned up interface

--- NumberClassifier/main.py
+++ NumberClassifier/main.py
@@ -1,12 +1,23 @@
 from read_data import readMNistImages, readMNistLabels
 from matplotlib import pyplot as plt
     
+# Plot a grayscale image
+def showImage(image, title = "", show = True):
+    plt.imshow(image, cmap='gray', vmin=0, vmax=255)
+    plt.title(title)
+    if show:
+        plt.show()
 
 if __name__ == "__main__":
     images = readMNistImages("data\\t10k-images.idx3-ubyte", magic_number=2051)
     labels = readMNistLabels("data\\t10k-labels.idx1-ubyte", magic_number=2049)
 
-    for i in range(len(images)):
-        plt.imshow(images[i], cmap="gray", vmin=0, vmax=255)
-        plt.title(labels[i])
-        plt.show()
+    # How many data points do we want to train our model on
+    data_sample_size = 10
+
+    # Make sure the data is valid
+    assert(data_sample_size < len(images))
+    assert(len(images) == len(labels))
+
+    for i in range(data_sample_size):
+        showImage(images[i], labels[i])
--- NumberClassifier/read_data.py
+++ NumberClassifier/read_data.py
@@ -1,29 +1,31 @@
 import struct
 import numpy as np
 
+def getNextField(data, format, idx):
+    datasize = struct.calcsize(format)
+    (observed_data,) = struct.unpack(format, data[idx : idx + datasize])
+    idx += datasize
+    return (observed_data, idx)
+    
+
 def readMNistLabels(filepath, magic_number):
     header_fields = [">i",  # 32-bit int : magic number
                      ">i"]  # 32-bit int : number of labels
 
-    idx = 0 # The current position in the binary file
 
     # Get the data from the file
     data = open(filepath, "rb").read()
 
-    # Start unpacking the data
-    datasize = struct.calcsize(header_fields[0])
+    # The current position in the binary file
+    idx = 0 
 
     # Get the magic number and make sure it matches
-    (observed_magic_number,) = struct.unpack(">i", data[idx : idx+datasize])
+    (observed_magic_number, idx) = getNextField(data, header_fields[0], idx)
     if observed_magic_number != magic_number:
         print("Something's wrong, the magic number should have been {} but was instead {}".format(magic_number, observed_magic_number))
-    idx += datasize
 
     # Determine how many images are in the dataset
-    datasize = struct.calcsize(header_fields[1])
-    (image_count,) = struct.unpack(">i", data[idx : idx+datasize])
-    print("Opened a file with {} images".format(image_count))
-    idx += datasize
+    (image_count, idx) = getNextField(data, header_fields[1], idx)
 
     # Get the labels for each image
     datasize = struct.calcsize(">" + "B"*image_count)
@@ -43,40 +45,29 @@ def readMNistImages(filepath, magic_number):
     # Get the data from the file
     data = open(filepath, "rb").read()
 
-    # Start unpacking the data
-    datasize = struct.calcsize(header_fields[0])
-
     # Get the magic number and make sure it matches
-    (observed_magic_number,) = struct.unpack(">i", data[idx : idx+datasize])
+    (observed_magic_number, idx) = getNextField(data, header_fields[0], idx)
     if observed_magic_number != magic_number:
         print("Something's wrong, the magic number should have been {} but was instead {}".format(magic_number, observed_magic_number))
-    idx += datasize
 
     # Determine how many images are in the dataset
-    datasize = struct.calcsize(header_fields[1])
-    (image_count,) = struct.unpack(">i", data[idx : idx+datasize])
-    print("Opened a file with {} images".format(image_count))
-    idx += datasize
-
-    # Get the row and column count for each image
-    datasize = struct.calcsize(header_fields[2])
-    (row_count,) = struct.unpack(header_fields[2], data[idx : idx+datasize])
-    idx += datasize
-
-    datasize = struct.calcsize(header_fields[3])
-    (column_count,) = struct.unpack(header_fields[3], data[idx : idx + datasize])
-    idx += datasize
-    print("Each image has {} rows and {} columns".format(row_count, column_count))
+    (image_count,  idx) = getNextField(data, header_fields[1], idx)
+    (row_count,    idx) = getNextField(data, header_fields[2], idx)
+    (column_count, idx) = getNextField(data, header_fields[3], idx)
+    print("Retrieved {} images with {} rows and {} columns".format(image_count, row_count, column_count))
 
     # Get the image data
     images = []
     image_format = ">" + "B"*row_count*column_count
     datasize = struct.calcsize(image_format)
+
     for _ in range(image_count):
-        new_image = np.zeros((row_count, column_count), np.uint8)
+        # Get the image data
         image_pixels = struct.unpack(image_format, data[idx : idx + datasize])
         idx += datasize
 
+        # Convert it to a numpy MxN matrix
+        new_image = np.zeros((row_count, column_count), np.uint8)
         for i in range(column_count):
             for j in range(row_count):
                 new_image[i,j] = 255 - image_pixels[i*row_count + j]
