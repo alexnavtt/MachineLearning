e3d070bbc59d44571f1f8f2dd5a6a9ac03ab76f3
alexnavtt <alexnavtt@utexas.edu>
2021-09-28 13:11:37 -0500

    Cleaned up step function

--- SignalSeparator/SignalSeparator.py
+++ SignalSeparator/SignalSeparator.py
@@ -1,5 +1,4 @@
 import numpy as np
-import random
 
 """
 Notes on the notes:
@@ -11,7 +10,7 @@ Notes on the notes:
     X = mixed signal matrix (m x t)
     W = linear combination inverse approximation (n x m)
     A = inv(W) = linear signal combination matrix (m x n)
-    Y = estimate of U (n x t)
+    Y = estimate of U = WX (n x t)
     Z = 1/(1 + e^-Y) (n x t)
 """
 
@@ -29,12 +28,12 @@ class SignalSeparator:
         self.signal_length = np.shape(data)[1]
         self.input_signal_count = np.shape(data)[0]
 
-    def isolateSignals(self, start_value = 0.1, step_size = 0.1):
+    def isolateSignals(self, start_value = 0.1, step_size = 0.1, max_count=1e4):
         self.W = np.random.rand(self.signal_count, self.input_signal_count)*start_value
 
         iter_count = 0
         change = 1
-        while change > 0.00000005 and (iter_count < 1e4):
+        while change > 0.000001 and (iter_count < max_count):
             iter_count += 1
             best_guess, change = self.stepByGradientDescent(step_size)
             print("\r", "Iterations: ", iter_count, "\t, Relative change: {:.8f}".format(change), end = "")
@@ -42,12 +41,20 @@ class SignalSeparator:
 
         return best_guess
 
+    def g(self, s):
+        return 1/(1 + np.exp(-s))
+                
 
     def stepByGradientDescent(self, step_size = 0.1):
+        # Get the individual terms
         Y = self.W @ self.mixed_signal
-        Z = 1/(1 + np.exp(-1.5*Y))
-        # delta_W = step_size * (np.identity(self.signal_count) + (1 - 2*Z)@Y.T) @ self.W
-        delta_W = step_size * (1 - 2*Z) @ Y.T @ self.W
-        diff = np.linalg.norm(delta_W)/np.linalg.norm(self.W)
+        I = np.identity(self.signal_count)
+        Z = self.g(Y)
+
+        # Calculate the step
+        delta_W = step_size * (I + (1 - 2 * Z) @ Y.T) @ self.W
+        change = np.linalg.norm(delta_W)/np.linalg.norm(self.W)
+
+        # Increment the W estimation and return
         self.W += delta_W
-        return Y, diff
+        return Y, change
