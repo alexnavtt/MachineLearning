fe7a628c1b122af043b2f62667a86b8f715f1fe2
alexnavtt <alexnavtt@utexas.edu>
2021-11-21 21:15:13 -0600

    Added copy method and type hints on x and y

--- ReinforcementLearning/object.py
+++ ReinforcementLearning/object.py
@@ -3,13 +3,21 @@ from matplotlib import pyplot as plt
 class Object():
 
     class Loc:
-        def __init__(self, x, y):
+        def __init__(self, x: int, y: int):
             self.x = x
             self.y = y
 
         def __eq__(self, other):
             return self.x == other.x and self.y == other.y
 
+        def __copy__(self):
+            class_type = self.__class__
+            copy = class_type.__new__(class_type)
+            copy.__dict__.update(self.__dict__)
+            return copy
+
+        def copy(self):
+            return self.__copy__()
 
     def __init__(self, x=0, y=0):
         self.loc = Object.Loc(x, y)
