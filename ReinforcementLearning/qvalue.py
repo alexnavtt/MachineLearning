cfa66330dc92db4475078c53a7bd81184c1d2dcb
alexnavtt <alexnavtt@utexas.edu>
2021-11-24 11:51:36 -0600

    move q value updates to module class

--- ReinforcementLearning/qvalue.py
+++ ReinforcementLearning/qvalue.py
@@ -5,8 +5,8 @@ def dummy(env):
     return 0
 
 class QCell:
-    discount_factor = 0.9
-    learning_rate   = 0.1
+    discount_factor = 0.99
+    learning_rate   = 0.5
 
     def __init__(self, num_actions, module):
         self._q_value = np.zeros(num_actions)
@@ -15,22 +15,22 @@ class QCell:
     def qValue(self, action: Action):
         return self._q_value[action.value]
 
-    def temporalDifference(self, next_q_cell, action_index, env):
-        agent_loc = env.agent.loc
+    # def temporalDifference(self, next_q_cell, action_index, env):
+    #     agent_loc = env.agent.loc
 
-        immediate_reward = self._parent_module.reward_func(env)
-        future_reward    = QCell.discount_factor * max(next_q_cell.q_value)
-        applied_q_val    = self._q_value[action_index]
+    #     immediate_reward = self._parent_module.reward_func(env)
+    #     future_reward    = QCell.discount_factor * max(next_q_cell.q_value)
+    #     applied_q_val    = self._q_value[action_index]
 
-        return immediate_reward + future_reward - applied_q_val
+    #     return immediate_reward + future_reward - applied_q_val
 
-    def updateQValue(self, action_index, next_q_cell, env):
-        TD = self.temporalDifference(next_q_cell, action_index, env)
-        self._q_value[action_index] += QCell.learning_rate * TD
+    # def updateQValue(self, action_index, next_q_cell, env):
+    #     TD = self.temporalDifference(next_q_cell, action_index, env)
+    #     self._q_value[action_index] += QCell.learning_rate * TD
 
-    def updateFatalQValue(self, action_index, penalty):
-        TD = penalty - self.qValue(action_index)
-        self._q_value[action_index] += QCell.learning_rate * TD
+    # def updateFatalQValue(self, action_index, penalty):
+    #     TD = penalty - self.qValue(action_index)
+    #     self._q_value[action_index] += QCell.learning_rate * TD
 
 
 class Module:
@@ -43,7 +43,6 @@ class Module:
 
         # Create the list of states for the Q-Learning module
         num_actions = len(actions)
-        self.states = [QCell(num_actions, module = self) for ii in range(num_states + 1)]
         self.q_table = [np.zeros(num_actions) for ii in range(num_states + 1)]
 
         # How this modules weighs compared to others
